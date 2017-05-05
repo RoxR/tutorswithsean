2ca020599c3774e2bc0c6cc25c43e7b52302d647
roxr <roxanaguinea@yahoo.com>
2017-05-05 22:41:23 +0200

    Secondcommit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,28 @@
+class CoursesController < ApplicationController
+  def index
+    end
+  end
+
+def new
+
+end
+
+def create
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+def show
+
+end
+
+def destroy
+
+end
